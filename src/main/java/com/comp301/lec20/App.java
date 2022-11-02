d9e46d9400449dec15d5dd2a469e6a69843bdeda
Ketan Mayer-Patel <kmp@cs.unc.edu>
2022-11-02 12:00:46 -0400

    Top level panes for scoreboard, instructions, and board.

--- src/main/java/com/comp301/lec20/App.java
+++ src/main/java/com/comp301/lec20/App.java
@@ -19,6 +19,18 @@ public class App extends Application {
     // Create new Pane objects to hold the UI components
     Pane layout = new VBox();
 
+    // The scoreboard goes at the top of the board
+    Pane scoreboard = new HBox();
+    layout.getChildren().add(scoreboard);
+
+    // Add the instructions to the layout
+    Label instructions = new Label("Join the numbers and get to the 2048 tile!");
+    layout.getChildren().add(instructions);
+
+    // The game board goes at the bottom of the layout
+    GridPane board = new GridPane();
+    layout.getChildren().add(board);
+
     // Set the Scene
     Scene scene = new Scene(layout, 350, 450);
     stage.setScene(scene);
