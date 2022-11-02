61c9d128cf57a93395269800aeb8fd754c772f85
Ketan Mayer-Patel <kmp@cs.unc.edu>
2022-11-02 11:53:25 -0400

    Took out everything to start from beginning.

--- src/main/java/com/comp301/lec20/App.java
+++ src/main/java/com/comp301/lec20/App.java
@@ -18,76 +18,9 @@ public class App extends Application {
 
     // Create new Pane objects to hold the UI components
     Pane layout = new VBox();
-    layout.getStyleClass().add("layout");
-
-    // The scoreboard goes at the top of the board
-    Pane scoreboard = new HBox();
-    scoreboard.getStyleClass().add("scoreboard");
-    layout.getChildren().add(scoreboard);
-
-    // The scoreboard has a logo
-    Pane logoContainer = new HBox();
-    Label logo = new Label("2048");
-    logo.getStyleClass().add("logo");
-    logoContainer.getChildren().add(logo);
-    HBox.setHgrow(logoContainer, Priority.ALWAYS);
-    scoreboard.getChildren().add(logoContainer);
-
-    // The scoreboard shows the current score
-    Pane score = new VBox();
-    score.getStyleClass().add("score");
-    Label scoreLabel = new Label("SCORE");
-    scoreLabel.getStyleClass().add("score-label");
-    score.getChildren().add(scoreLabel);
-    Label scoreValue = new Label("2268");
-    scoreValue.getStyleClass().add("score-value");
-    score.getChildren().add(scoreValue);
-    scoreboard.getChildren().add(score);
-
-    // The scoreboard also shows the best score
-    Pane best = new VBox();
-    best.getStyleClass().add("score");
-    Label bestLabel = new Label("BEST");
-    bestLabel.getStyleClass().add("score-label");
-    best.getChildren().add(bestLabel);
-    Label bestValue = new Label("2268");
-    bestValue.getStyleClass().add("score-value");
-    best.getChildren().add(bestValue);
-    scoreboard.getChildren().add(best);
-
-    // Add the instructions to the layout
-    Label instructions = new Label("Join the numbers and get to the 2048 tile!");
-    instructions.getStyleClass().add("instructions");
-    layout.getChildren().add(instructions);
-
-    // The game board goes at the bottom of the layout
-    GridPane board = new GridPane();
-    board.setHgap(10);
-    board.setVgap(10);
-    board.getStyleClass().add("board");
-    layout.getChildren().add(board);
-
-    // Fill up the board with tiles
-    board.add(makeTile(0), 0, 0);
-    board.add(makeTile(2), 1, 0);
-    board.add(makeTile(8), 2, 0);
-    board.add(makeTile(4), 3, 0);
-    board.add(makeTile(16), 0, 1);
-    board.add(makeTile(64), 1, 1);
-    board.add(makeTile(4), 2, 1);
-    board.add(makeTile(2), 3, 1);
-    board.add(makeTile(8), 0, 2);
-    board.add(makeTile(4), 1, 2);
-    board.add(makeTile(32), 2, 2);
-    board.add(makeTile(4), 3, 2);
-    board.add(makeTile(2), 0, 3);
-    board.add(makeTile(256), 1, 3);
-    board.add(makeTile(8), 2 ,3);
-    board.add(makeTile(2), 3, 3);
 
     // Set the Scene
     Scene scene = new Scene(layout, 350, 450);
-    scene.getStylesheets().add("style/main.css");
     stage.setScene(scene);
 
     // Show the Stage on the screen
@@ -96,17 +29,5 @@ public class App extends Application {
 
   public static void main(String[] args) {
     launch();
-  }
-
-  private static Label makeTile(int num) {
-    Label tile;
-    if (num == 0) {
-      tile = new Label();
-    } else {
-      tile = new Label(String.valueOf(num));
-    }
-    tile.getStyleClass().add("tile");
-    tile.getStyleClass().add("tile-" + num);
-    return tile;
   }
 }
